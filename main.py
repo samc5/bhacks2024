ae8d5411b7d11209bbffe853fc9c345ae20a2bde
samc5 <samcowan1968@gmail.com>
2024-11-02 14:56:31 -0400

    made it so main.py calls cloudflare api, prints is in the top dialog box

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Flask app with one endpoint
 from flask import Flask, jsonify, request, render_template
+import tester
 
 app = Flask(__name__)
 
@@ -7,7 +8,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return render_template("main.html")
+    prompt = """Via Metaculus, Kamala Harris currently has a 52% chance of winning and Donald Trump has a 45% chance of winning. With the results in NE-02, the odds of a tie have increased to around 3%.
+    Donald Trump has been holding an election night rally all night in North Carolina, via AP. He has been leading in the state, but early returns have not distinguished a front runner, says the NYT Needle, which puts NC at 60% for Trump.
+    """
+    live_summary = tester.get_ai_summary(prompt)
+
+    return render_template("main.html", live_summary=live_summary)
 
 
     
