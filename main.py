104f855dbd79931e777db8cfb1c4003c59465cf9
samc5 <samcowan1968@gmail.com>
2024-11-02 13:06:52 -0400

    added flask app shell

---
+++ main.py
@@ -0,0 +1,16 @@
+# Flask app with one endpoint
+from flask import Flask, jsonify, request, render_template
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def main():
+    return render_template("main.html")
+
+
+    
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
